7e05f8b1926d515d95616d6497a84ce5a27356b8
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-09-12 23:15:01 +0100

    Implemented hashing for level 3 security

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema ({
    
    });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
    
@@ -47,7 +47,7 @@ app.get("/login", function(req , res){
 app.post("/register", function(req, res){
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err,){
@@ -61,7 +61,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
