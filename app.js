18ca389e7814c6bb2b7374647ef385796d506a79
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-09-13 12:52:16 +0100

    implemented bcrypt for level 4 security.

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -25,8 +26,6 @@ const userSchema = new mongoose.Schema ({
     password: String
    
    });
-
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
    
@@ -45,30 +44,41 @@ app.get("/login", function(req , res){
 // Register new user.
 
 app.post("/register", function(req, res){
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save(function(err,){
-        if (err) {
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err,){
+            if (err) {
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
 });
+   
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = (req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
             console.log(err);
         }else{
-            if (foundUser.password === password){
-                res.render("secrets");
+            if (foundUser){
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+        
+                if (result === true){
+                    res.render("secrets");
+
+                } else{
+                    console.log(err);
+                }
+                }); 
             }
         }
     });
